560cb712d5e6652dadb18c70b76cb1951255c1a5
lukesnc <35816797+lukesnc@users.noreply.github.com>
2025-04-16 01:45:03 -0400

    add fonts to installed packages

--- scripts/i3setup.sh
+++ scripts/i3setup.sh
@@ -2,7 +2,7 @@
 # Run from the root of the repo ie.
 # $ ./scripts/i3setup.sh
 
-sudo pacman -S i3 alacritty rofi brightnessctl network-manager-applet
+sudo pacman -S i3 alacritty rofi brightnessctl network-manager-applet gnu-free-fonts noto-fonts
 
 mkdir -v -p ~/.config/i3
 cp -v ./.config/i3/config ~/.config/i3/
@@ -21,3 +21,4 @@ echo "Optionally install"
 echo "    redshift for blue light filtering"
 echo "    feh for desktop wallpaper"
 echo "    fcitx5-im and fcitx5-mozc for Japanese keyboard input"
+echo "    blueman for bluetooth support (includes blueman-applet)"

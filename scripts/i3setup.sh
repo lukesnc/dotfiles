31184af4fbf376dd0d4d6d03397b68522c66b020
lukesnc <35816797+lukesnc@users.noreply.github.com>
2025-05-05 22:01:18 -0400

    i3setup script: massive improvements

--- scripts/i3setup.sh
+++ scripts/i3setup.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
-# Run from the root of the repo ie.
-# Only works on Arch rn
-# $ ./scripts/i3setup.sh
+# Only works with pacman rn 
+
+set -e
 
 sudo pacman -S i3 xss-lock alacritty rofi brightnessctl network-manager-applet gnu-free-fonts noto-fonts
 
-mkdir -v -p ~/.config/i3
-cp -v ./.config/i3/config ~/.config/i3/config
+repodir=$(realpath "$(dirname "$0")/..")
+mkdir -vp ~/.config
+cp -vr "$repodir/.config/i3" ~/.config
+cp -vr "$repodir/.config/i3status" ~/.config
+cp -vr "$repodir/.config/rofi" ~/.config
+cp -v "$repodir/.xinitrc" ~/.xinitrc
 
-mkdir -v -p ~/.config/i3status
-cp -v ./.config/i3status/config ~/.config/i3status/config
-
-mkdir -v -p ~/.config/rofi
-cp -v ./.config/rofi/config.rasi ~/.config/rofi/config.rasi
-
-cp -v ./.xinitrc ~/.xinitrc
-
-echo 'if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-  exec startx
-fi' >> ~/.bash_profile
-
-echo "Done."
-echo "Optionally install"
-echo "    redshift for blue light filtering"
-echo "    feh for desktop wallpaper"
-echo "    fcitx5-im and fcitx5-mozc for Japanese keyboard input"
-echo "    blueman for bluetooth support (includes blueman-applet)"
+cat << EOF
+Done.
+Optionally install
+    redshift for blue light filtering
+    feh for desktop wallpaper
+    fcitx5-im and fcitx5-mozc for Japanese keyboard input
+    blueman for bluetooth support (includes blueman-applet)
+EOF
